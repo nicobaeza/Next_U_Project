a3abab08c981019b875eb36ea87e19367f1540b0
Ico Nico <Nicolas@172-0-12-121.lightspeed.rcsntx.sbcglobal.net>
2017-07-07 12:40:31 +1200

    comentary

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text = "Hola \(nameTextField.text!)"
+        view.endEditing(true)
+    }
+// Comentary
 
 }
 
